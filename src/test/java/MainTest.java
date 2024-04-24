d54e80cd01285c8d511cae153a6309f21ae31659
“Shutov <zahar.shutov02@gmail.com>
2024-04-24 14:35:15 +0600

    test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,8 +1,10 @@
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.junit.Test;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class MainTest {
 
